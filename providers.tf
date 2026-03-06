c4b7381e5daacc277a76c50a16146e0b167cd06f
michael.sundalskliev <michael@LAPTOP-A0211M7D.localdomain>
2026-03-06 22:52:55 +0000

    lock terraform to 1.12 and update providers to latest versions

--- providers.tf
+++ providers.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = "~> 1.12.0"
 
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.23"
+      version = "~> 3.0"
     }
     kind = {
       source  = "tehcyx/kind"
-      version = "~> 0.2"
+      version = "~> 0.11"
     }
   }
 }
